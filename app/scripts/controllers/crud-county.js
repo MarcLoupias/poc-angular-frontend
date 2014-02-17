ac1fe5e9b6c544d53b4d58c8301c754fa8968539
marc.loupias <pro@marc-loupias.fr>
2014-02-17 09:29:01 +0100

    working version for update ng-grid data

--- app/scripts/controllers/crud-county.js
+++ app/scripts/controllers/crud-county.js
@@ -35,66 +35,75 @@ angular.module('pocAngularFrontendApp')
             }
         }, true);
 
-        var cellNameEditable
-            =   '<cell-template model=COL_FIELD input=COL_FIELD entity=row.entity></cell-template>' +
-            'COL_FIELD = {{row.getProperty(col.field)}} row.entity = {{row.entity}}';
+        $scope.fnUnchangedCode = function() {
+            return true;
+        };
 
-        var cellNameDisplay
-            = '<div class="ngCellText" ng-class="col.colIndex()">{{row.getProperty(col.field)}}'
-            + ' COL_FIELD = {{row.getProperty(col.field)}} row.entity = {{row.entity}}</div>';
+        $scope.fnValidCode = function(entity) {
+            countyService.putCounty(entity).then(
+                function () {
+                    angularUiAlertService.addSuccessAlert('Le département ' + entity.name + ' a été mis à jour.');
+                }
+            );
+        };
+
+        $scope.fnInvalidCode = function(entity) {
+            angularUiAlertService.addWarningAlert('Saisie invalide. Le département '+ entity.name + ' n\'a pas été mis à jour.');
+            if (!$scope.$$phase) {
+                $scope.$apply();
+            }
+        };
+
+        var cellNameEditable =
+            '<cell-template-input-text model=COL_FIELD required="true" min-length="3" ' +
+                'max-length="30" ' +
+                'unchanged-callback="fnUnchangedCode()" ' +
+                'valid-callback="fnValidCode(row.entity)" ' +
+                'invalid-callback="fnInvalidCode(row.entity)" ' +
+                '></cell-template-input-text>';
+
+        var cellCodeEditable =
+            '<cell-template-input-text model=COL_FIELD required="true" min-length="1" ' +
+                'max-length="3" pattern="\'/^[0-9]+$/\'" pattern-error-msg="\'Nombre obligatoire\'" ' +
+                'unchanged-callback="fnUnchangedCode()" ' +
+                'valid-callback="fnValidCode(row.entity)" ' +
+                'invalid-callback="fnInvalidCode(row.entity)" ' +
+                '>' +
+                '</cell-template-input-text>';
+
+        var cellNameDisplay =
+            '<div class="ngCellText" ng-class="col.colIndex()">{{row.getProperty(col.field)}}</div>';
+
+        var cellCodeDisplay =
+            '<div class="ngCellText" ng-class="col.colIndex()">{{row.getProperty(col.field)}}</div>';
 
         $scope.gridOptionsCrudCounty = {
+            i18n: 'fr',
             data: 'myData',
             multiSelect: false,
             enableCellSelection: true,
             enableRowSelection: false,
             enableCellEditOnFocus: false,
-            rowHeight: 60,
             columnDefs: [
                 {field:'id', displayName:'Id', visible: false},
-                {field:'code', displayName:'Code', enableCellEdit:true},
                 {
-                    field:'name', displayName:'Name', enableCellEdit:true,
-                    cellTemplate: cellNameDisplay,
-                    editableCellTemplate: cellNameEditable
+                    field:'code', displayName:'Code', enableCellEdit:true,
+                    cellTemplate: cellCodeDisplay, editableCellTemplate: cellCodeEditable
+                },
+                {
+                    field:'name', displayName:'Nom', enableCellEdit:true,
+                    cellTemplate: cellNameDisplay, editableCellTemplate: cellNameEditable
                 }
             ],
             enablePaging: true,
             showFooter: true,
             totalServerItems:'totalServerItems',
-            pagingOptions: $scope.pagingOptions/*,
-            afterSelectionChange: function() {
-                console.log('afterSelectionChange callback fired');
-            },
-            beforeSelectionChange: function(row) {
-                console.log('beforeSelectionChange callback fired arg = ' + JSON.stringify(row.entity));
-                $scope.oldEntity = angular.copy(row.entity);
-
-                return true;
-            }*/
+            pagingOptions: $scope.pagingOptions
         };
 
         $scope.pagingOptions.currentPage = 1;
+    })
 
-        /*$scope.$on('ngGridEventEndCellEdit', function(event) {
-
-            console.log('ngGridEventEndCellEdit fired');
-
-            if(event.targetScope.validForm) {
-                if(!angular.equals($scope.oldEntity, event.targetScope.entity)) {
-                    countyService.putCounty(event.targetScope.entity).then(function(res) {
-                        angularUiAlertService.addSuccessAlert('Le département ' + event.targetScope.entity.name + ' a été modifié avec succès.');
-                    });
-                }
-            } else {
-                event.targetScope.entity = $scope.oldEntity;
-            }
-        });*/
-
-        $scope.$on('ngGridEventEndCellEdit', function(event) {
-            console.log('ngGridEventEndCellEdit fired');
-            //var entity = event.targetScope.entity;
-            //event.currentScope.entity = event.targetScope.entity;
-            /*var entity = event.currentScope.entity;*/
-         });
+    .controller('CrudCountyNewCountyCtrl', function ($scope, countyService, angularUiAlertService) {
+        $scope.isCollapsed = true;
     });
--- app/scripts/directives/my-ng-grid-cells-templates.js
+++ app/scripts/directives/my-ng-grid-cells-templates.js
@@ -1,48 +1,158 @@
 'use strict';
 
 angular.module('pocAngularFrontendApp')
-    .directive('cellTemplate', function () {
+
+/**
+ * Directive taken from ng-grid ng-input (v2.0.7)
+ */
+    .directive('myNgInput', [function () {
+        return {
+            require: 'ngModel',
+            link: function (scope, elm, attrs, ngModel) {
+                // Store the initial cell value so we can reset to it if need be
+                var oldCellValue;
+                var dereg = scope.$watch('ngModel', function () {
+                    oldCellValue = ngModel.$modelValue;
+                    dereg(); // only run this watch once, we don't want to overwrite our stored value when the input changes
+                });
+
+                elm.bind('keydown', function (evt) {
+                    switch (evt.keyCode) {
+                        case 37: // Left arrow
+                        case 38: // Up arrow
+                        case 39: // Right arrow
+                        case 40: // Down arrow
+                            evt.stopPropagation();
+                            break;
+                        case 27: // Esc (reset to old value)
+                            if (!scope.$$phase) {
+                                scope.$apply(function () {
+                                    ngModel.$setViewValue(oldCellValue);
+                                    elm.blur();
+                                });
+                            }
+                            break;
+                        case 13: // Enter (Leave Field)
+                            if (scope.enableCellEditOnFocus && scope.totalFilteredItemsLength() - 1 > scope.row.rowIndex && scope.row.rowIndex > 0 || scope.enableCellEdit) {
+                                elm.blur();
+                            }
+                            break;
+                    }
+
+                    return true;
+                });
+
+                elm.bind('click', function (evt) {
+                    evt.stopPropagation();
+                });
+
+                elm.bind('mousedown', function (evt) {
+                    evt.stopPropagation();
+                });
+
+                scope.$on('ngGridEventStartCellEdit', function () {
+                    elm.focus();
+                    elm.select();
+                });
+
+                angular.element(elm).bind('blur', function () {
+
+                    if (angular.equals(ngModel.$modelValue, oldCellValue)) {
+                        scope.$emit('ngGridEventEndCellEdit_Unchanged');
+                    } else {
+                        if (scope.myForm.$valid === false) {
+                            if (!scope.$$phase) {
+                                scope.$apply(function () {
+                                    ngModel.$setViewValue(oldCellValue);
+                                });
+                            }
+                            scope.$emit('ngGridEventEndCellEdit_Invalid');
+
+                        } else {
+                            scope.$emit('ngGridEventEndCellEdit_Valid');
+                        }
+                    }
+
+                    scope.$emit('ngGridEventEndCellEdit');
+                });
+            }
+        };
+    }])
+
+    .directive('cellTemplateInputText', function () {
 
         var cellTemplate =
-        '<div><form name="myForm" class="simple-form" novalidate>' +
-            //'<input type="text" name="\'field\' + col.index" ng-class="\'colt\' + col.index" ng-input="COL_FIELD" ng-model="COL_FIELD" required/>' +
-            '<input type="text" name="myField" ng-input="localInput" ng-model="localModel" entity="entity" required/>' +
-            '<span ng-show="myForm.myField.$error.required"> REQUIRED</span>' +
-        '</form></div>';
+            '<div><form name="myForm" class="simple-form" novalidate>' +
+                '<input type="text" name="myField" my-ng-input="localModel" ng-model="localModel" ng-minlength="{{formMinLength}}" ng-maxlength="{{formMaxLength}}" ng-required="{{formRequired}}" ng-pattern="{{formPattern}}"/>' +
+                '<span ng-show="myForm.myField.$error.required" class="label label-danger"> Obligatoire</span>' +
+                '<span ng-show="myForm.myField.$error.minlength" class="label label-danger"> {{ formMinLength }} caractères min</span>' +
+                '<span ng-show="myForm.myField.$error.maxlength" class="label label-danger"> {{ formMaxLength }} caractères max</span>' +
+                '<span ng-show="myForm.myField.$error.pattern" class="label label-danger"> {{ formPatternErrorMsg }}</span>' +
+            '</form></div>';
 
         return {
             template: cellTemplate,
             restrict: 'E',
             scope: {
-                localModel:'=model',
-                localInput:'=input',
-                entity:'=entity'
+                formRequired: '=required',
+                formMinLength: '=minLength',
+                formMaxLength: '=maxLength',
+                formPattern: '=pattern',
+                formPatternErrorMsg: '=patternErrorMsg',
+                localModel: '=model',
+                unchangedCallback: '&',
+                validCallback: '&',
+                invalidCallback: '&'
             },
-            controller: function ($scope, countyService, angularUiAlertService) {
-
-                $scope.$on('ngGridEventStartCellEdit', function (event) {
-                    console.log('cellTemplate controller - ngGridEventStartCellEdit fired');
-
-                    $scope.oldEntity = angular.copy(event.currentScope.entity);
-                    $scope.oldValue = angular.copy(event.currentScope.localModel);
+            controller: function ($scope) {
+                $scope.$on('ngGridEventEndCellEdit_Unchanged', function () {
+                    $scope.unchangedCallback();
                 });
 
-                $scope.$on('ngGridEventEndCellEdit', function(event) {
+                $scope.$on('ngGridEventEndCellEdit_Valid', function () {
+                    $scope.validCallback();
+                });
 
-                    console.log('ngGridEventEndCellEdit fired');
+                $scope.$on('ngGridEventEndCellEdit_Invalid', function () {
+                    $scope.invalidCallback();
+                });
+            }
+        };
+    })
 
-                    if(event.currentScope.myForm.$valid) {
-                        if(!angular.equals($scope.oldEntity, event.currentScope.entity)) {
-                            countyService.putCounty(event.currentScope.entity).then(function(res) {
-                                angularUiAlertService.addSuccessAlert('Le département ' + event.targetScope.entity.name + ' a été modifié avec succès.');
-                            });
-                        }
-                    } else {
+    .directive('cellTemplateInputNumber', function () {
 
-                        $scope.localModel = angular.copy($scope.oldValue);
-                        $scope.localInput = angular.copy($scope.oldValue);
-                        $scope.entity = angular.copy($scope.oldEntity);
-                    }
+        var cellTemplate =
+            '<div><form name="myForm" class="simple-form" novalidate>' +
+                '<input type="number" name="myField" my-ng-input="localModel" ng-model="localModel" min="{{formMin}}" max="{{formMax}}" ng-required="{{formRequired}}"/>' +
+                '<span ng-show="myForm.myField.$error.required" class="label label-danger"> Obligatoire</span>' +
+                '<span ng-show="myForm.myField.$error.min" class="label label-danger"> {{formMin}} min</span>' +
+                '<span ng-show="myForm.myField.$error.max" class="label label-danger"> {{formMax}} max</span>' +
+                '</form></div>';
+
+        return {
+            template: cellTemplate,
+            restrict: 'E',
+            scope: {
+                formRequired: '=required',
+                formMin: '=min',
+                formMax: '=max',
+                localModel: '=model',
+                unchangedCallback: '&',
+                validCallback: '&',
+                invalidCallback: '&'
+            },
+            controller: function ($scope) {
+                $scope.$on('ngGridEventEndCellEdit_Unchanged', function () {
+                    $scope.unchangedCallback();
+                });
+
+                $scope.$on('ngGridEventEndCellEdit_Valid', function () {
+                    $scope.validCallback();
+                });
+
+                $scope.$on('ngGridEventEndCellEdit_Invalid', function () {
+                    $scope.invalidCallback();
                 });
             }
         };
